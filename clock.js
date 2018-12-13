1a8434c3bcc033fe2de67b44b8a3ca258537de92
torvalds <deandb2015@gmail.com>
2018-12-14 01:10:38 +0800

    Format js file with js_format plugin(SublimeText3)

--- clock.js
+++ clock.js
@@ -10,14 +10,14 @@ function drawBackground() {
   ctx.translate(r, r);
   ctx.beginPath();
   ctx.lineWidth = 10 * rem;
-  ctx.arc(0, 0, r - ctx.lineWidth / 2, 0, 2*Math.PI, false);
+  ctx.arc(0, 0, r - ctx.lineWidth / 2, 0, 2 * Math.PI, false);
   ctx.stroke();
 
   var hourNumbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
-  ctx.font = 18 * rem +  "px Arial";
+  ctx.font = 18 * rem + "px Arial";
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
-  hourNumbers.forEach( function(number, index) {
+  hourNumbers.forEach(function(number, index) {
     var rad = 2 * Math.PI / 12 * index;
     var x = Math.cos(rad) * (r - 30 * rem);
     var y = Math.sin(rad) * (r - 30 * rem);
@@ -29,7 +29,7 @@ function drawBackground() {
     var x = Math.cos(rad) * (r - 18 * rem);
     var y = Math.sin(rad) * (r - 18 * rem);
     ctx.beginPath();
-    if (i % 5 ===0) {
+    if (i % 5 === 0) {
       ctx.fillStyle = '#000';
       ctx.arc(x, y, 2 * rem, 0, 2 * Math.PI, false);
     } else {
@@ -85,7 +85,7 @@ function drawSecond(second) {
 function drawDot() {
   ctx.beginPath();
   ctx.fillStyle = '#fff';
-  ctx.arc(0, 0, 3 * rem, 0, 2*Math.PI, false);
+  ctx.arc(0, 0, 3 * rem, 0, 2 * Math.PI, false);
   ctx.fill();
 }
 
